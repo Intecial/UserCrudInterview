f2f0023664394ca49c8cdaa2759f5969e08ff4c2
Louis Juliano <ljuliano30@gmail.com>
2023-05-24 01:38:11 +0800

    Removed not required constructors

--- interviewAPI/Model/User.cs
+++ interviewAPI/Model/User.cs
@@ -17,23 +17,4 @@ public class User{
         this.phoneNumber = phoneNumber;
         this.age = age;
     }
-
-    // public User (string name, string email){
-    //     this.name = name;
-    //     this.email = email;
-    //     this.Id = guid.ToString();
-    // }
-
-    // public User (string name, string email, string phoneNumber){
-    //     this.name = name;
-    //     this.email = email;
-    //     this.Id = guid.ToString();
-    //     this.phoneNumber = phoneNumber;
-    // }
-    // public User (string name, string email, int age){
-    //     this.name = name;
-    //     this.email = email;
-    //     this.Id = guid.ToString();
-    //     this.age = age;
-    // }
 }
